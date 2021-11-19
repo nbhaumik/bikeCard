c3237bbf4ff9a9a4ed20730de1e3d0d7f66d10aa
Navoniloy Bhaumik <nbhaumik@suyatitech.local>
2021-11-19 15:42:24 +0530

    First

--- force-app/main/default/lwc/bikeCard/bikeCard.js
+++ force-app/main/default/lwc/bikeCard/bikeCard.js
@@ -1,7 +1,7 @@
 import { LightningElement } from 'lwc';
 export default class BikeCard extends LightningElement {
    name = 'Electra X4';
-   description = 'A sweet bike built for comfort.';
+   description = 'A sweets bike built for comfort.';
    category = 'Mountain';
    material = 'Steel';
    price = '$2,700';
